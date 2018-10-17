04a1dccdb4615baa09e3d5cb1d330bd8ce94a9fb
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-16 18:15:18 -0700

    create components

--- client/navbar.jsx
+++ client/navbar.jsx
@@ -6,12 +6,6 @@ const Navbar = () => {
     <div id="navbar">
       <Link to="/" className="name">Donguk Kim</Link>
       <div className="tabs">
-        {/* <div className="tabs-projects"><Link to="/projects" className="tabs-link"/>Projects</div>
-        <div className="tabs-skills"><Link to="/skills" className="tabs-link"/>Skills</div>
-        <div className="tabs-interests"><Link to="/interests" className="tabs-link"/>Interests</div>
-        <div className="tabs-education"><Link to="/education" className="tabs-link"/>Education</div> */}
-
-
         <Link to="/projects" className="tabs-projects">Projects</Link>
         <Link to="/skills" className="tabs-skills">Skills</Link>
         <Link to="/interests" className="tabs-interests">Interests</Link>
