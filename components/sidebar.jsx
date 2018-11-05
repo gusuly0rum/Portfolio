d5fdccf22f6ba8dc8ab817d3656f0c805d7a6385
gusuly0rum <donguk.kim1112@gmail.com>
2018-11-04 21:08:56 -0800

    add resume and angellist to sidebar

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -7,10 +7,12 @@ const Sidebar = () => {
       <div className="photo-profile"></div>
       <div className="links-sidebar">
         <a href="https://github.com/gusuly0rum" target="_blank" className="link-github"><div className="photo-github"></div>GitHub</a>
-        <a href="https://www.linkedin.com/in/donguk-kim/" target="_blank" className="link-linkedin"><div className="photo-linkedin"></div>LinkedIn</a>
+        <a href="https://www.linkedin.com/in/donguk-kim" target="_blank" className="link-linkedin"><div className="photo-linkedin"></div>LinkedIn</a>
+        <a href="https://angel.co/dongukkim" target="_blank" className="link-angel"><div className="photo-angel"></div>AngelList</a>
+        <a href="resume.pdf" target="_blank" className="link-resume"><div className="photo-resume"></div>Résumé</a>
         <Link to="/contact" className="link-email"><div className="photo-email"></div>Email</Link>
       </div>
-      <div className="date-updated">Last updated: 17 October 2018</div>
+      <div className="date-updated">Last updated: 04 Nov 2018</div>
     </div>
   );
 };
